51e20937a9e2ada133810f8ee0e3578e878fc57e
Rory Stokes <rstokes@spartaglobal.com>
2019-10-06 17:56:46 +0100

    created connection class

---
+++ db_connection.py
@@ -0,0 +1,15 @@
+import pyodbc
+
+class Connection:
+    def __init__(self, server, database, username, password):
+        self.server = server
+        self.database = database
+        self.username = username
+        self.password = password
+        self.conn_nwdb = pyodbc.connect(
+            'DRIVER={ODBC Driver 17 for SQL Server};SERVER= ' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
+        self.cursor = self.conn_nwdb.cursor()
+
+
+    def filter_query(self, query):
+        return self.cursor.execute(query)
